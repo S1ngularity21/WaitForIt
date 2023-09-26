779213cd794f0d06a6c24b8591578ed90aa466af
Pablo Sebastian Vivanco <66503033+PabloSebasVivanco@users.noreply.github.com>
2023-09-26 15:47:12 -0700

    Add checks for inputs arg

--- bin/wait/Wait.cpp
+++ bin/wait/Wait.cpp
@@ -10,26 +10,36 @@
 Wait::Wait(int argc, char **argv)
 	: POSIXApplication(argc, argv)
 {
+	// Constructor for the Wait class. It initializes the application with command-line arguments.
 	parser().setDescription("Wait for program to finish");
 	parser().registerPositional("ID", "Stop program execution");
 }
 
 Wait::~Wait()
 {
-	//test
+	// Destructor for the Wait class. It can be used for cleanup if needed.
 }
 
 Wait::Result Wait::exec()
 {
 	int status;
-	pid_t pid = atoi(arguments().get("ID"));
+    const char *idArg = arguments().get("ID");
+	char *endptr;
+    pid_t pid = strtol(idArg, &endptr, 10);
 
-	if(waitpid(pid, &status, 0) == (pid_t) -1) {
-		ERROR("Status Error");
-		return IOError;
-	}
-
-	return Success;
-
+    // Check if the ID argument is provided and is a valid integer.
+    if (!idArg || !*idArg) {
+        ERROR("Invalid ID argument");
+        return IOError;
+    }
+    if (*endptr != '\0') {
+        ERROR("Invalid ID argument");
+        return IOError;
+    }
+    // Wait for the specified process ID to finish.
+    if (waitpid(pid, &status, 0) == -1) {
+        ERROR("Status Error: " << strerror(errno));
+        return IOError;
+    }
+    return Success;	
 }
-
