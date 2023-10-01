a909896041f6404600c6285592dcfe9a7dde87fc
amogh-opsverse <99157069+amogh-opsverse@users.noreply.github.com>
2023-09-30 22:09:12 -0700

    add logging statements

--- bin/wait/Wait.cpp
+++ bin/wait/Wait.cpp
@@ -11,8 +11,8 @@ Wait::Wait(int argc, char **argv)
 	: POSIXApplication(argc, argv)
 {
 	// Constructor for the Wait class. It initializes the application with command-line arguments.
-	parser().setDescription("Wait for program to finish");
-	parser().registerPositional("ID", "Stop program execution");
+	parser().setDescription("Wait for a specific process to finish. Provide a valid process ID as an argument.");
+	parser().registerPositional("ID", "Process ID to wait for");
 }
 
 Wait::~Wait()
@@ -23,11 +23,16 @@ Wait::~Wait()
 Wait::Result Wait::exec()
 {
 	int status;
+    
     const char *idArg = arguments().get("ID");
 	char *endptr;
-    pid_t pid = strtol(idArg, &endptr, 10);
+    pid_t pid = strtol(idArg, &endptr, 10);                                                                                                                                                                                 
+    //pid_t pid;
+
 
     // Check if the ID argument is provided and is a valid integer.
+    
+    
     if (!idArg || !*idArg) {
         ERROR("Invalid ID argument");
         return IOError;
@@ -35,11 +40,18 @@ Wait::Result Wait::exec()
     if (*endptr != '\0') {
         ERROR("Invalid ID argument");
         return IOError;
+    
     }
+
+    // Wait for the specified process ID to finish.
+    printf("Waiting for process %d to finish...\n", pid);
     // Wait for the specified process ID to finish.
     if (waitpid(pid, &status, 0) == -1) {
+        printf("Finished waiting for process %d, but an error occurred: %s\n", pid, strerror(errno));
         ERROR("Status Error: " << strerror(errno));
         return IOError;
     }
+    printf("Finished waiting for process %d.\n", pid);
+
     return Success;	
 }
